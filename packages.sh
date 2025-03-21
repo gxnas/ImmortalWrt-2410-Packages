d9e2d719d49c65ff64001743e4843d6b76f377c6
gxnas <52808122+gxnas@users.noreply.github.com>
2025-03-21 13:43:24 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -4,23 +4,8 @@ shopt -s extglob
 rm -rfv !(LICENSE|README.md|packages.sh)
 shopt -u extglob
 
-function git_sparse_clone() {
-branch="$1" rurl="$2" localdir="$3" && shift 3
-git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
-cd $localdir
-git sparse-checkout init --cone
-git sparse-checkout set $@
-mv -n $@ ../
-cd ..
-rm -rf $localdir
-}
-
-function mvdir() {
-mv -n `find $1/* -maxdepth 0 -type d` ./
-rm -rf $1
-}
-
-git_sparse_clone openwrt-24.10 https://github.com/immortalwrt/luci immortalwrt-luci applications/*
+git clone --depth=1 -b openwrt-24.10 https://github.com/immortalwrt/luci
+mv luci/applications/* .
 
 
 rm -rf ./*/.git & rm -rf ./*/.gitattributes
