a6d2cf637b3d870d06bb4ccbe92f348a86987479
gxnas <52808122+gxnas@users.noreply.github.com>
2025-03-21 13:49:41 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -6,7 +6,7 @@ shopt -u extglob
 
 git clone --depth=1 -b openwrt-24.10 https://github.com/immortalwrt/luci
 mv luci/applications/* .
-
+rm -rf luci
 
 rm -rf ./*/.git & rm -rf ./*/.gitattributes
 rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
