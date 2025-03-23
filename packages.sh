2bdef5396c8e939fa43cf2403f74fde17c0a85b5
gxnas <52808122+gxnas@users.noreply.github.com>
2025-03-23 15:03:19 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -4,11 +4,14 @@ shopt -s extglob
 rm -rfv !(LICENSE|README.md|packages.sh)
 shopt -u extglob
 
+git clone --depth=1 -b openwrt-24.10 https://github.com/immortalwrt/luci.git
+mv -n luci/applications/* ./
+rm -rf luci
+
 git clone --depth=1 https://github.com/sirpdboy/luci-app-partexp.git
 git clone --depth=1 https://github.com/sirpdboy/luci-app-poweroffdevice.git
 git clone --depth=1 https://github.com/tty228/luci-app-wechatpush.git
 git clone --depth=1 https://github.com/asvow/luci-app-tailscale.git
-git clone --depth=1 -b openwrt-24.10 https://github.com/immortalwrt/luci.git && mv luci/applications/* ./ && rm -rf luci
 git clone --depth=1 https://github.com/linkease/istore.git && mv -n istore/luci/luci-app-store ./ && rm -rf istore
 git clone --depth=1 https://github.com/danchexiaoyang/luci-app-onliner.git && mv luci-app-onliner danchexiaoyang-onliner && mv -n danchexiaoyang-onliner/luci-app-onliner ./ && rm -rf danchexiaoyang-onliner
 
